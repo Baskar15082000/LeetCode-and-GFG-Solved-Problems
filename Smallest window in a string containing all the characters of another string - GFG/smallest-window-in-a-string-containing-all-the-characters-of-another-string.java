916e38e2ac1c3961ef3e6632a26677c5231fc7f3
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-07-18 09:59:06 +0530

    Added solution - LeetHub

--- Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.java
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.java
@@ -24,71 +24,44 @@ class Solution
 {
     //Function to find the smallest window in the string s consisting
     //of all the characters of string p.
-    public static String smallestWindow(String s, String t)
+    public static boolean matching(HashMap<Character,Integer> map,HashMap<Character,Integer> curr){
+       for(char val:map.keySet()){
+			
+			if(!curr.containsKey(val))return false;
+			if(curr.get(val)<map.get(val))return false;
+			
+		}
+		return true;
+    }
+    public static String smallestWindow(String s, String p)
     {
         // Your code here
-         		HashMap<Character,Integer> map1 = new HashMap<>();
-		for(int i=0;i<t.length();i++){
-			char ch=t.charAt(i);
-			if(map1.containsKey(ch)){
-				map1.put(ch,map1.get(ch)+1);
-			}
-			else{
-				map1.put(ch,1);
-			}
-		}
-		String ans="";
-		HashMap<Character,Integer> map2=new HashMap<>();
-		int match=t.length();
-		int count=0;
-		int l=0;
-		int r=0;
-		while(true){
-			boolean f1=false;
-			boolean f2=false;
+      		HashMap<Character,Integer> map=new HashMap<>();
+			HashMap<Character,Integer> curr=new HashMap<>();
+		for(int i=0;i<p.length();i++){
+			char ch=p.charAt(i);
+			map.put(ch,map.getOrDefault(ch,0)+1);
 			
-			if(r<s.length() && match>count){
-				char ch=s.charAt(r);
-				if(map1.containsKey(ch)){
-					if(map2.getOrDefault(ch,0)<map1.get(ch)){
-						count++;
-					}
-				}
-				if(map2.containsKey(ch)){
-					map2.put(ch,map2.get(ch)+1);
-				}
-				else{
-					map2.put(ch,1);
-				}
-				//System.out.print("Ff");
-				f1=true;
-                
-			 r++;
-			}
-			else if(l<r && match==count){
-				String sub=s.substring(l,r);
-				if(ans=="" || (ans.length()>sub.length())){
-					ans=sub;
-					//System.out.print(ans);
-				}
-				
-				char cha=s.charAt(l);
-				if(map1.containsKey(cha)){
-					if(map2.get(cha)<=map1.get(cha)){
-						 count--;
-					}
-				}
-				map2.put(cha,map2.get(cha)-1);
-				if(map2.get(cha)==0){
-					map2.remove(cha); 
-				}
-				l++;
-				f2=true;
-			
-			}
-			if(f1==false && f2==false) break;
 		}
-		if(ans=="") return "-1";
+		int sp=0;
+		int ep=0;
+		int length=Integer.MAX_VALUE;
+		String ans="-1";
+		while(ep<s.length()){
+			char ch=s.charAt(ep);
+			curr.put(ch,curr.getOrDefault(ch,0)+1);
+			while(matching(map,curr)){
+				if(length>(ep-sp+1)){
+				length=Math.min(length,ep-sp+1);
+					
+				ans=s.substring(sp,ep+1);
+				}
+				char c=s.charAt(sp);
+				curr.put(c,curr.get(c)-1);
+				sp++;
+			}
+			ep++;
+		}
 		return ans;
     }
 }
