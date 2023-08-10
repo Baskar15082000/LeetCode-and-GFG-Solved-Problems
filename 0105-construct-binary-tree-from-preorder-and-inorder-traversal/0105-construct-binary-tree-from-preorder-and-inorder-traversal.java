a578a71c78af40440561c85cdb2fae64eedd7257
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-08-10 12:21:57 +0530

    Time: 3 ms (45.12%), Space: 43.4 MB (49.58%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,23 +14,22 @@
  * }
  */
 class Solution {
-    public TreeNode cons(int pre[],int prest,int preend,int in[],int inst,int inend){
-        if(prest>preend||inst>inend){
-            return null;
-        }
-        TreeNode root=new TreeNode(pre[prest]);
-        int i=inst;
+    public TreeNode cons(int pre[],int ps,int pe,int in[],int is,int ie){
+        if(ps>pe || is>ie)return null;
+        TreeNode root=new TreeNode(pre[ps]);
         int count=0;
+        int i=is;
         while(true){
             if(root.val==in[i]){
                 break;
             }
             i++;
             count++;
+           
         }
-        root.left=cons(pre,prest+1,prest+count,in,inst,i-1);
-        root.right=cons(pre,prest+count+1,preend,in,i+1,inend);
-        return root;
+         root.left=cons(pre,ps+1,ps+count,in,is,i-1);
+         root.right=cons(pre,ps+count+1,pe,in,i+1,ie);
+         return root;
     }
     public TreeNode buildTree(int[] pre, int[] in) {
         return cons(pre,0,pre.length-1,in,0,in.length-1);
