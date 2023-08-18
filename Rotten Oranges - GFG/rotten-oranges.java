ace0153080bf1bc6cedfc6821e748e610d168ca3
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-08-18 13:23:43 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.java
+++ Rotten Oranges - GFG/rotten-oranges.java
@@ -31,55 +31,55 @@ class GFG
 
 class Solution
 {
-    static class pair{
+    //Function to find minimum time required to rot all oranges. 
+   class pair{
         int row;
         int col;
-		int time;
-        pair(int row,int col ,int time){
+        int time;
+        pair(int row,int col,int time){
             this.row=row;
             this.col=col;
-			this.time=time;
+            this.time=time;
         }
     }
-    //Function to find minimum time required to rot all oranges. 
     public int orangesRotting(int[][] grid)
+    
     {
-        // Code here 
         int n=grid.length;
-		 int m=grid[0].length;
-		int vis[][]=new int [n][m]; 
-		 int countfresh=0;
-		 Queue<pair> q=new LinkedList<>();
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(grid[i][j]==2){
-					vis[i][j]=2;
-					q.add(new pair(i,j,0));
-				}
-				if(grid[i][j]==1){
-					countfresh++;
-				}
-			}
-		}
-		int rotten=0;
-		 int time=0;
-		while(q.size()>0){
-			pair p=q.remove();
-			time=Math.max(time,p.time);
-			int r[]={-1,0,1,0};
-			int c[]={0,-1,0,1};
-			for(int i=0;i<4;i++){
-				int cr=p.row+r[i];
-				int cc=p.col+c[i];
-				if(cr>=0 && cc>=0 && cr<n && cc<m && grid[cr][cc]==1 && vis[cr][cc]==0 ){
-					vis[cr][cc]=2;
-					rotten++;
-					q.add(new pair(cr,cc,p.time+1));
-					
-				}
-			}
-		}
-		 if(rotten<countfresh)return -1;
-		 return time;
+        int m=grid[0].length;
+        int ans[][]=new int[n][m];
+        Queue<pair> q=new LinkedList<>();
+        int ft=0;
+         int delrow[]={-1,0,1,0};
+        int delcol[]={0,-1,0,1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    ans[i][j]=2;
+                    q.add(new pair(i,j,0));
+                }
+                if(grid[i][j]==1)ft++;
+            }
+        }
+        int rt=0;
+        int time=0;
+    
+        while(q.size()>0){
+            pair p=q.remove();
+            time=Math.max(p.time,time);
+            int r=p.row;
+            int c=p.col;
+            for(int i=0;i<4;i++){
+                int cr=r+delrow[i];
+                int cc=c+delcol[i];
+                if(cr>=0 && cr<n && cc>=0 && cc<m && grid[cr][cc]==1 && ans[cr][cc]==0){
+                    rt++;
+                    ans[cr][cc]=2;
+                    q.add(new pair(cr,cc,p.time+1));
+                }
+            }
+        }
+        if(ft!=rt)return -1;
+        return time;
     }
 }
