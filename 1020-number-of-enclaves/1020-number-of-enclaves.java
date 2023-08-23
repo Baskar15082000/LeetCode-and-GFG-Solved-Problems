6c844c4565fab20c00192da9bc4237299303b94d
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-08-23 13:15:46 +0530

    Time: 10 ms (52.36%), Space: 54.1 MB (83.93%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,30 +1,63 @@
 class Solution {
-    public void Deleteme(int i,int j,int grid[][]){
-	  if(i<0||j<0||i==grid.length||j==grid[0].length||grid[i][j]==0){
-		  return;
-	  }
-	  grid[i][j]=0;
-	  Deleteme(i+1,j,grid);
-	  Deleteme(i-1,j,grid);
-	  Deleteme(i,j-1,grid);
-	  Deleteme(i,j+1,grid);
-	  
-  }
-    public int numEnclaves(int[][] grid) {
-         for(int j=0;j<grid[0].length;j++){
-		  if(grid[0][j]==1)Deleteme(0,j,grid);
-		  if(grid[grid.length-1][j]==1)Deleteme(grid.length-1,j,grid);
-	  }
-	  for(int i=0;i<grid.length;i++){
-		 if(grid[i][0]==1) Deleteme(i,0,grid);
-		 if(grid[i][(grid[0].length-1)]==1) Deleteme(i,grid[0].length-1,grid);
-	  }
-	  int count=0;
-	  for(int i=0;i<grid.length;i++){
-		  for(int j=0;j<grid[0].length;j++){
-			  if(grid[i][j]==1)count++;
-		  }
-	  }
-	  return count;
+     class pair{
+        int row;
+        int col;
+        pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public int numEnclaves(int[][] board) {
+        int n=board.length;
+        int m=board[0].length;
+        Queue<pair>q=new LinkedList<>();
+        int vis[][]=new int[n][m];
+        for(int i=0;i<m;i++){
+            if(vis[0][i]==0 && board[0][i]==1){
+                vis[0][i]=1;
+                q.add(new pair(0,i));
+            }
+            if(vis[n-1][i]==0 && board[n-1][i]==1){
+                vis[n-1][i]=1;
+                q.add(new pair(n-1,i));
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(vis[i][0] ==0 && board[i][0]==1){
+                vis[i][0]=1;
+                q.add(new pair(i,0));
+            }
+            if(vis[i][m-1]==0 && board[i][m-1]==1){
+                vis[i][m-1]=1;
+                q.add(new pair(i,m-1));
+            }
+        }
+        int r[]={-1,0,1,0};
+        int c[]={0,-1,0,1};
+        while(q.size()>0){
+            pair p=q.remove();
+            int row=p.row;
+            int col=p.col;
+           // if(vis[p.row][p.col]==1)continue;
+          //   vis[p.row][p.col]=1;
+             for(int i=0;i<4;i++){
+               int cr=row+r[i];
+               int cc=col+c[i];
+               if(cr>=0 && cc>=0 && cr<n && cc<m && board[cr][cc]==1 && vis[cr][cc]==0){
+                   vis[cr][cc]=1;
+                   q.add(new pair(cr,cc));
+                
+               }
+             }
+        }
+        int count=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==1 && vis[i][j]==0){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
