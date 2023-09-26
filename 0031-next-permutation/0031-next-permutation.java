0916a0e0cbeb398eb2d2b25ebfc413ce2be1faa8
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-09-26 13:28:15 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (75.68%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+       int n = nums.length;
+        
+        // Step 1: Find the first element from the right such that nums[i] < nums[i+1]
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        // Step 2: If no such element is found, reverse the array and return
+        if (i == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        
+        // Step 3: Find the smallest element to the right of i that is greater than nums[i]
+        int j = n - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        
+        // Step 4: Swap nums[i] with nums[j]
+        swap(nums, i, j);
+        
+        // Step 5: Reverse the subarray to the right of i
+        reverse(nums, i + 1, n - 1);
+    }
+    
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    } 
+    
+}
