eb7d2b7a66c0b71c508070d3c6c7a6bd6d7af4be
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-09-26 12:54:01 +0530

    Time: 16 ms (5.42%), Space: 70.1 MB (99.07%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        Queue<Integer> q1=new LinkedList<>();
+         Queue<Integer> q2=new LinkedList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                q1.add(nums[i]);
+            }
+            else{
+                q2.add(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                nums[i]=q1.remove();
+            }
+            else{
+                nums[i]=q2.remove();
+            }
+        }
+        return nums;
+        
+    }
+}
