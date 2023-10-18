09583d71c58158a13a7fdd7d365258b13db48ad9
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-10-18 17:59:06 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (82.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,6 +1,6 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-       int n = nums.length;
+        int n = nums.length;
         
         // Step 1: Find the first element from the right such that nums[i] < nums[i+1]
         int i = n - 2;
