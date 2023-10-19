8d179a61b58614b8571be125374c36cd33b12c7c
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-10-19 10:36:16 +0530

    Time: 48 ms (35.03%), Space: 127.8 MB (18.96%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,64 +1,35 @@
 class LRUCache {
-    int capacity;
-    HashMap<Integer,Node>cache;
-    Node head=new Node(0,0);
-    Node tail=new Node(0,0);
-    
+    int cap;
+    LinkedHashMap<Integer,Integer> cache;
 
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        cache=new HashMap<>();
-        head.next=tail;
-        tail.pre=head;
+        this.cap=capacity;
+        cache=new LinkedHashMap<>();
     }
     
     public int get(int key) {
-        if(cache.containsKey(key)){
-            Node node=cache.get(key);
-            remove(node);
-            insert(node);
-            return node.data;
+        if(!cache.containsKey(key)){
+            return -1;
         }
-        return -1;
+        int val=cache.get(key);
+        cache.remove(key);
+        cache.put(key,val);
+        return val;
+       
     }
     
     public void put(int key, int value) {
-        if(cache.containsKey(key)){
-          remove(cache.get(key));
-          
-        }
-        if(capacity==cache.size()){
-            remove(tail.pre);
-        }
-          
-            Node newnode =new Node(key,value);
-           
-        insert(newnode);
-    }
-    public void insert(Node newnode){
-        Node next=head.next;
-        head.next=newnode;
-        newnode.pre=head;
-        newnode.next=next;
-        next.pre=newnode;
-        cache.put(newnode.key,newnode);
+         if(cache.containsKey(key)){
+             cache.remove(key);
+             cache.put(key,value);
+             return ;
+         }
+         if(cache.size()>=cap){
+             int deletekey=cache.keySet().iterator().next();
+             cache.remove(deletekey);
+         }
+        cache.put(key,value);
         
-    }
-     public void remove(Node newnode){
-         
-            newnode.pre.next=newnode.next;
-            newnode.next.pre=newnode.pre;
-            cache.remove(newnode.key);
-    }
-    class Node{
-        Node next;
-        Node pre;
-        int key;
-        int data;
-        Node(int key,int data){
-            this.key=key;
-            this.data=data;
-        }
     }
 }
 
