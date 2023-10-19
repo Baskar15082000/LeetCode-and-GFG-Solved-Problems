0e15e941fbf34982f7304646d58b7944efc5fcce
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-10-19 10:26:34 +0530

    Time: 62 ms (38.29%), Space: 124.9 MB (57.17%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,55 +1,40 @@
 class LFUCache {
 
 	private int cap;
-	// Key , Value 
-
-
 	private int min = 1;
-
-	Map<Integer,Integer> keyValueMap = new HashMap<>();
-	// Key , keyFrequencyMap
+	Map<Integer,Integer> keyValueMap= new HashMap<>();
 	Map<Integer,Integer> keyFrequencyMap = new HashMap<>();
-	// keyFrequencyMap, Set of keys in ordered format 
-	// Why ordered so that in case of collision least recently used is keyToBeEvicteded
 	Map<Integer,LinkedHashSet<Integer>> freqKeysMap = new HashMap<>();
 
 	public LFUCache(int capacity) {
 		cap = capacity;
 		freqKeysMap.put(1,new LinkedHashSet<>());
 	}
-    // O(1)
-	public int get(int key) {
 
-		if(!keyValueMap.containsKey(key)) {
+	public int get(int key) {
+        if(!keyValueMap.containsKey(key)) {
 			return -1;
 		}
-
-		int frequency = keyFrequencyMap.get(key);
-		// remove the key from the previous frequency 
-		freqKeysMap.get(frequency).remove(key);
-
-		if(frequency==min && freqKeysMap.get(frequency).size()==0) {
+        int frequency = keyFrequencyMap.get(key);
+        keyFrequencyMap.put(key,frequency+1);
+        freqKeysMap.get(frequency).remove(key);
+        if(frequency==min && freqKeysMap.get(frequency).size()==0) {
             	min++;
 		} 
 		if(!freqKeysMap.containsKey(frequency+1)) {
 			freqKeysMap.put(frequency + 1, new LinkedHashSet<>());
 		}
-		// add the key corresponding to freq+1
 		freqKeysMap.get(frequency+1).add(key);
-		// increment the frequency by one for the current key
-		keyFrequencyMap.put(key,frequency+1);
-
-		return keyValueMap.get(key);
+	    return keyValueMap.get(key);
 	}
 
-    // O(1)
-	public void put(int key, int value) {
+    public void put(int key, int value) {
 		if(cap<=0)
 			return;
 		if(keyValueMap.containsKey(key))
 		{
 			keyValueMap.put(key,value);
-			get(key);
+            get(key);
 			return;
 		}
 		if(keyValueMap.size()>=cap)
