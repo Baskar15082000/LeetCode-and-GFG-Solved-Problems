6d8356114e5873055bc04b2981c92e6f607c2038
Baskar15082000 <134188943+Baskar15082000@users.noreply.github.com>
2023-11-01 11:45:52 +0530

    Time: 1 ms (100.00%), Space: 43.7 MB (67.33%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -28,25 +28,14 @@ class Solution {
         if(head==null)return true;
         if(root==null)return false;
         
-         return isSubPathFromRoot(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+        return dfs(head,root)||isSubPath(head,root.left)||isSubPath(head,root.right);
     }
-//     boolean dfs(ListNode head, TreeNode root){
-//         if(head==null)return true;
-//         if(root==null)return false;
+    boolean dfs(ListNode head, TreeNode root){
+        if(head==null)return true;
+        if(root==null)return false;
         
-//         if(root.val!=head.val)return false;
-//         return dfs(head.next,root.left)||dfs(head.next,root.right);
-//     }
-    
-     private boolean isSubPathFromRoot(ListNode head, TreeNode root) {
-        if (head == null) {
-            return true; // We have reached the end of the linked list.
-        }
-        if (root == null || head.val != root.val) {
-            return false; // Mismatch in values or no more nodes in the tree.
-        }
-
-        return isSubPathFromRoot(head.next, root.left) || isSubPathFromRoot(head.next, root.right);
+        if(root.val!=head.val)return false;
+        return dfs(head.next,root.left)||dfs(head.next,root.right);
     }
     
 }
